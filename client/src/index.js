46fcf2e6cc5c3db3c67f2ee11d79c9aa1a77a5dc
Christie Lum <christielum02@gmail.com>
2022-10-16 02:14:29 -0700

    installed chakra-ui

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  backgroundColor: '#f5f5dc'
+  // colors: {
+  //   brand: {
+  //     100: "#f7fafc",
+  //     // ...
+  //     900: "#1a202c",
+  //   },
+  // },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
