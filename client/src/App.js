d7e82b29352a40e34cc3f3f5eb47d1f3df24963b
Christie Lum <christielum02@gmail.com>
2022-10-16 04:07:22 -0700

    deleted navigation links

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { gapi } from 'gapi-script';
-import Student from './components/studentHome';
-import teacher from './components/teacherHome';
+
 import {
 	ChakraProvider,
 	Center,
@@ -13,14 +12,6 @@ import {
 	HStack,
   } from '@chakra-ui/react'
   import theme from './theme';
-
-import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	Navigate,
-	Link
-} from "react-router-dom";
 
 const clientId = '195055295608-gjvnf37g5n4jdero49bod908e6p40igs.apps.googleusercontent.com';
 
@@ -70,61 +61,36 @@ function App() {
 		setProfile(null);
 	};
 
-	const teacherPage = () => {
-		return {teacher};
-	};
-
-	const studentPage = () => {
-		return {student};
-	};
-
 	return (
 		<ChakraProvider theme={theme}>
 		<div className="App">
       		<img className="logo" src={group}/>
 
-			<>
-			<Router>
-				<Routes>
-          			<Route path="/teacherHome" component={<Teacher/>} />
-					<Route path="/studentHome" component={<Student/>} />
-					<Navigate to="/" />
-				</Routes>
-			</Router>
-			</>
-
 			<div>
 				{profile ? (
 					<div>
-						<div>
-							{/* Todo: Should only ask who are you? for first sign in */}
-							<Center marginTop={'175px'} marginBottom={'25px'} style={{fontSize: '50px'}}>
-								Who are you?
-							</Center>
-							<Center>
-							<HStack spacing={8}>
-								<Link>
-								</Link>
-								<Button onClick={studentPage} colorScheme='teal' width='300px'>Teacher</Button>
-
-								{/* <Button onClick={studentPage} colorScheme='teal' width='300px'>Teacher</Button> */}
-								{/* Todo: Add teacher or student to the cockroachDB */}
-								<Button onClick={teacherPage} colorScheme='teal' width='300px'>Student</Button>
-							</HStack>
-							</Center>
-							<GoogleLogout 
-								clientId={clientId} 
-								render={renderProps => (
-									<Button onClick={renderProps.onClick} marginTop={'75px'} colorScheme='teal'>log out</Button>
-								)}
-								onLogoutSuccess={logOut} 
-							/>
-						</div>
+						{/* Todo: Should only ask who are you? for first sign in */}
+						<Center marginTop={'175px'} marginBottom={'25px'} style={{fontSize: '50px'}}>
+							Who are you?
+						</Center>
+						<Center>
+						<HStack spacing={8}>
+							<Button colorScheme='teal' width='300px'>Teacher</Button>
+							{/* Todo: Add teacher or student to the cockroachDB */}
+							<Button colorScheme='teal' width='300px'>Student</Button>
+						</HStack>
+						</Center>
+						<GoogleLogout 
+							clientId={clientId} 
+							render={renderProps => (
+								<Button onClick={renderProps.onClick} marginTop={'75px'} colorScheme='teal'>log out</Button>
+							)}
+							onLogoutSuccess={logOut} 
+						/>
 					</div>
 
 				) : (
 					<div>
-						<div>
 						<Center marginTop={'175px'} marginBottom={'25px'}>
 						<b style={{fontSize: '50px'}}>groUP</b> 
 						</Center>
@@ -141,7 +107,6 @@ function App() {
 							cookiePolicy={'single_host_origin'}
 							isSignedIn={true}
 						/>
-						</div>
 					</div>
 					
 				)}
