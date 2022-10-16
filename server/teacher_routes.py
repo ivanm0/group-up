c902b265e3fb197048628deb6238cbe13418050f
Ivan Mao <ivanmao0@gmail.com>
2022-10-16 07:29:34 -0700

    update teacher routes

--- server/teacher_routes.py
+++ server/teacher_routes.py
@@ -6,43 +6,6 @@ from werkzeug.utils import redirect
 from models import Teacher
 
 
-<<<<<<< HEAD
-@app.route("/teacher", methods=['GET', 'POST'])
-def teacher():
-   if request.method == 'POST':
-      data = request.json
-      def callback(session):
-         teacher = Teacher(
-            id=data['id'],
-            firstname=data['first'],
-            lastname=data['last']
-         )
-         session.add(teacher)
-         return {"status": True}
-   else:
-      def callback(session):
-         teachers = session.query(Teacher).all()
-         result = list(map(lambda teacher: {'id': teacher.id,
-                                          'firstname': teacher.firstname,
-                                          'lastname': teacher.lastname,
-                                          },
-                           teachers))
-         return {"teachers": result}
-   return run_transaction(snmaker, callback)
-
-
-# @app.route("/teachers")
-# def teachers():
-#    def callback(session):
-#       teachers = session.query(Teacher).all()
-#       result = list(map(lambda teacher: {'id': teacher.id,
-#                                        'firstname': teacher.firstname,
-#                                        'lastname': teacher.lastname,
-#                                        },
-#                         teachers))
-#       return {"teachers": result}
-#    return run_transaction(snmaker, callback)
-=======
 
 @app.route("/teachers", methods=['GET', 'POST'])
 def teachers():
@@ -70,5 +33,4 @@ def teachers():
       
 
 
->>>>>>> 361d7a3bd0e268c5ceaef7f6b5a204f2c3243749
 
