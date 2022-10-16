71fa16a30134576ca9c1b82a77cb4d57b815d0a1
Ivan Mao <ivanmao0@gmail.com>
2022-10-15 23:49:51 -0700

    add google sign in

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,88 @@
 import logo from './logo.svg';
-import axios from "axios";
+import axios from 'axios';
 import './App.css';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
+const clientId = '195055295608-gjvnf37g5n4jdero49bod908e6p40igs.apps.googleusercontent.com';
 
 function App() {
+	const [ test, setTest ] = useState('hi');
 
-  const [test, setTest] = useState("hi");
+	useEffect(() => {
+		const initClient = () => {
+			gapi.client.init({
+				clientId: clientId,
+				scope: ''
+			});
+		};
+		gapi.load('client:auth2', initClient);
+	});
 
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: "/test"
-    }).then(res => {
-      console.log(res);
-      setTest(res.data.msg);
-    });
-  })
+	useEffect(() => {
+		axios({
+			method: 'GET',
+			url: '/test'
+		}).then((res) => {
+			console.log(res);
+			setTest(res.data.msg);
+		});
+	});
 
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <p>{test}</p>
-    </div>
-  );
+	const [ profile, setProfile ] = useState([]);
+	useEffect(() => {
+		const initClient = () => {
+			gapi.client.init({
+				clientId: clientId,
+				scope: ''
+			});
+		};
+		gapi.load('client:auth2', initClient);
+	});
+
+	const onSuccess = (res) => {
+		setProfile(res.profileObj);
+	};
+
+	const onFailure = (err) => {
+		console.log('failed', err);
+	};
+
+	const logOut = () => {
+		setProfile(null);
+	};
+
+	return (
+		<div className="App">
+			<div>
+				<h2>React Google Login</h2>
+				<br />
+				<br />
+				{profile ? (
+					<div>
+						<img src={profile.imageUrl} alt="user image" />
+						<h3>User Logged in</h3>
+						<p>Name: {profile.name}</p>
+						<p>Email Address: {profile.email}</p>
+						<br />
+						<br />
+						<GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+					</div>
+				) : (
+					<GoogleLogin
+						clientId={clientId}
+						buttonText="Sign in with Google"
+						onSuccess={onSuccess}
+						onFailure={onFailure}
+						cookiePolicy={'single_host_origin'}
+						isSignedIn={true}
+					/>
+				)}
+			</div>
+			<p>{test}</p>
+		</div>
+	);
 }
 
 export default App;
