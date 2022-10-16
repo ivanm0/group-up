af9be2fd331abe4ef1147919e067094bcb684b99
ahjune1 <paul1224.ahn@gmail.com>
2022-10-16 03:48:49 -0700

    added classes

--- server/app.py
+++ server/app.py
@@ -5,18 +5,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 from cockroachdb.sqlalchemy import run_transaction
 from werkzeug.utils import redirect
+from models import *
 
 app = Flask(__name__)
 conn_string = 'cockroachdb://ivan:Jr6FxlLgfFYt3DykWC_YWg@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/groupup?sslmode=verify-full&options=--cluster%3Dfurry-mammal-5848'
 engine = create_engine(conn_string, convert_unicode=True)
 sessionmaker = sessionmaker(bind=engine)
-Base = declarative_base()
-
-class Teacher(Base):
-    __tablename__ = 'teachers'
-    id = Column(UUID, primary_key=True)
-    firstname = Column(String)
-    lastname = Column(String)
 
 @app.route("/test")
 def hello_world():
@@ -34,6 +28,7 @@ def add_teacher():
       session.add(teacher)
    run_transaction(sessionmaker, callback)
    return redirect(url_for("teachers"))
+
 
 @app.route("/teachers")
 def teachers():
